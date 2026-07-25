119808a10fb2d8018db61ca87a668ad828d5d5ac
Tchauuw <evan.gueguen030602@gmail.com>
2026-07-25 14:30:58 +0200

    Crud des entités nécessaires

---
+++ src/Controller/Admin/LigneVenteCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\LigneVente;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class LigneVenteCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return LigneVente::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
